de91aca2fd39b9fda5f62da29e0779234d7eba74
ShubhamKumar3010 <shubham8969305655@gmail.com>
2020-05-31 22:18:27 +0530

    LeetCode Problem added.

--- Code Runner.cpp
+++ Code Runner.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <unordered_map>
 using namespace std;
 int findComplement(int num);
 vector<int> twoSum(vector<int>& nums,int target);
@@ -8,16 +10,177 @@ bool kLengthApart(vector<int>& nums, int k);
 int isPrefixOfWord(string sentence,string searchWord);
 int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime);
 vector<string> buildArray(vector<int>& target, int n);
+int searchInsert(vector<int>& nums, int target);
+string destCity(vector<vector<string>>& paths);
+int lastStoneWeight(vector<int>& stones);
+int findLucky(vector<int>& arr);
+int maxPower(string s);
+string addBinary(string a, string b);
+int addDigits(int num);
+string addStrings(string num1, string num2);
+vector<int> addToArrayForm(vector<int>& A, int K);
 int main(){
-    vector<int> v;
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(4);
-    vector<string> out = buildArray(v,4);
+    vector<int> v {9,9,9,9,9,9,9,9,9,9};
+    vector<int> out =addToArrayForm(v,1);
     for(int i=0;i<out.size();i++){
         cout<<out[i]<<endl;
     }
-    return 0;
+}
+//For a non-negative integer X, the array-form of X is an array of its digits in left to right order.  For example, if X = 1231, then the array form is [1,2,3,1].
+//Given the array-form A of a non-negative integer X, return the array-form of the integer X+K.
+//Input: A = [1,2,0,0], K = 34
+//Output: [1,2,3,4]
+//Explanation: 1200 + 34 = 1234
+vector<int> addToArrayForm(vector<int>& A, int K) {
+    int sum=0;
+    for(int i=0;i<A.size();i++){
+        sum=sum*10+A[i];
+    }
+    int v = sum + K;
+    vector<int> out;
+    while(v>0){
+        int r = v%10;
+        out.push_back(r);
+        v=v/10;
+    }
+    reverse(out.begin(),out.end());
+    return out;
+}
+//Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+//Example:
+//Input: 38
+//Output: 2
+//Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
+//Since 2 has only one digit, return it.
+int addDigits(int num) {
+    if(num<10 && num>=0){
+        return num;
+    }
+
+}
+string addBinary(string a, string b) {
+    string result="";
+    int s=0;
+    int i = a.size()-1,j = b.size()-1;
+    while (i>=0 || j>=0 || s == 1){
+        s+=((i >= 0) ? a[i] - '0' : 0);
+        s+=((j >= 0) ? b[j] - '0' : 0);
+
+        result = char (s%2+'0') + result ;
+
+        s/=2;
+        i--;
+        j--;
+    }
+    return result;
+}
+//Given a string s, the power of the string is the maximum length of a non-empty substring that contains only one unique character.
+//Return the power of the string.
+//Input: s = "leetcode"
+//Output: 2
+int maxPower(string s) {
+        int count,max=1;
+        for(int i=0;i<s.length();i++){
+            count = 1;
+            while(s[i] == s[i+1] && i<s.length()){
+                count++;
+                i++;
+            }
+            max = std::max(max,count);
+        }
+        return max;
+}
+int lastStoneWeight(vector<int>& stones) {
+    if(stones.size()==0){
+        return 0;
+    }
+    int x,y;
+    while(stones.size()>0){
+        if(stones.size()==0){
+            return 0;
+        }
+        sort(stones.begin(),stones.end());
+        x = stones.back();
+        stones.pop_back();
+        y = stones.back();
+        stones.pop_back();
+        if(x!=y){
+            x = x - y;
+            stones.push_back(x);
+        }
+    }
+    return stones[0];
+}
+//Given an array of integers arr, a lucky integer is an integer which has a frequency in the array equal to its value.
+//Return a lucky integer in the array. If there are multiple lucky integers return the largest of them. If there is no lucky integer return -1.
+int findLucky(vector<int>& arr) {
+    vector<int> out;
+    for(int i=0;i<arr.size();i++){
+        int count=0;
+        for(int j=0;j<arr.size();j++){
+            if(arr[i]==arr[j]){
+                count++;
+            }
+        }
+        if(arr[i]==count){
+            out.push_back(arr[i]);
+        }
+    }
+    int max;
+    if(out.size()>0){
+         max=out[0];
+        for(int i=0;i<out.size();i++){
+            if(max<out[i]){
+                max=out[i];
+            }
+        }
+    }else{
+        return -1;
+    }
+    return max;
+}
+
+//You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi.
+// Return the destination city, that is, the city without any path outgoing to another city.
+//It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+string destCity(vector<vector<string>>& paths) {
+    vector<string> v;
+    v.push_back(paths[0][1]);
+    v.push_back(paths[1][1]);
+    v.push_back(paths[2][1]);
+        int i=0;
+        while(i<3){
+            int count=0;
+            for(int j=0;j<paths.size();j++){
+                if(v[i]!=paths[j][0]) {
+                    count++;
+                }
+            }
+            if(count==paths.size()){
+                return v[i];
+            }
+            i++;
+        }
+}
+//Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+//You may assume no duplicates in the array.
+//Input: [1,3,5,6], 5
+//Output: 2
+int searchInsert(vector<int>& nums, int target) {
+    int low=0,high=nums.size()-1;
+//  As in question it is for sorted array so the best thing comes here is binary Search;
+    while(low<=high){
+        int mid=(low+high+1)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(nums[mid]>target){
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return low;
 }
 //Given an array target and an integer n. In each iteration, you will read a number from  list = {1,2,3..., n}.
 //Build the target array using the following operations:
